ab6079bc3a50a074d1f37e53543a39d2943cf83f
Fallen_Breath <yqx791125833@gmail.com>
2019-11-14 00:32:44 +0800

    add TileEntityListLogger and arrange CunkDebugLogger's code

---
+++ src/main/java/carpet/logging/logHelpers/TileEntityListLogHelper.java
@@ -0,0 +1,50 @@
+package carpet.logging.logHelpers;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import com.google.common.collect.Lists;
+
+import carpet.logging.LoggerRegistry;
+import carpet.utils.Messenger;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.ITextComponent;
+
+public class TileEntityListLogHelper {
+    private static List<ITextComponent> messages = Lists.newArrayList();
+    
+    public static void log(long gameTime, String msg, BlockPos pos)
+    {
+        TileEntityListLogHelper.messages.add(Messenger.c(
+                "g [" + gameTime + "] ",
+                "w " + msg + " ",
+                Messenger.tp("w", pos),
+                "g  at ", 
+                "e " + BlockUpdatesLogHelper.getTickStage()
+                ));
+    }
+    
+    public static void log(long gameTime, String msg, TileEntity te)
+    {
+        TileEntityListLogHelper.log(gameTime, msg, te.getPos());
+    }
+        
+    public static void flush()
+    {
+        LoggerRegistry.getLogger("tileentitylist").log( () -> 
+        {
+            List<ITextComponent> comp = new ArrayList<>();
+            Iterator<ITextComponent> iterator = TileEntityListLogHelper.messages.iterator();
+            while (iterator.hasNext()) 
+            {
+                ITextComponent message = iterator.next();
+                iterator.remove();
+                comp.add(message);
+            }
+            return comp.toArray(new ITextComponent[0]);
+        });
+    }
+    
+}
