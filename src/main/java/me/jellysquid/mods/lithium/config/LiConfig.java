db869754c5b6c2bac64b7e61315bc02419361edf
X-Green <2271174177@qq.com>
2020-08-16 10:32:05 +0800

    Config, World, Maths

---
+++ src/main/java/me/jellysquid/mods/lithium/config/LiConfig.java
@@ -0,0 +1,30 @@
+package me.jellysquid.mods.lithium.config;
+
+public class LiConfig {
+
+
+    /**
+     * Class changed:
+     * {@link net.minecraft.world.WorldServer}
+     *
+     */
+    public static class World {
+        /**
+         * {@link me.jellysquid.mods.lithium.scheduler.LithiumServerTickScheduler}
+         */
+        public static boolean world_useLithiumTickScheduler = true;
+
+    }
+
+    /**
+     * Class changed:
+     * {@link net.minecraft.util.math.AxisAlignedBB}
+     * {@link net.minecraft.util.EnumFacing}
+     * {@link net.minecraft.util.AxisRotation}
+     */
+    public static class Maths {
+
+    }
+
+
+}
