5634a21e371c5b52cb61d851aff59ebfb8a1b796
Fallen_Breath <yqx791125833@gmail.com>
2020-10-23 21:37:37 +0800

    IGNORE_CLASS_PATH logic in StackTraceDeobfuscator

--- src/main/java/carpet/microtick/utils/StackTraceDeobfuscator.java
+++ src/main/java/carpet/microtick/utils/StackTraceDeobfuscator.java
@@ -1,17 +1,25 @@
 package carpet.microtick.utils;
 
-import com.google.common.collect.Maps;
+import com.google.common.collect.Lists;
 
-import java.util.Map;
+import java.util.List;
 
 public class StackTraceDeobfuscator
 {
-	private static final String MAPPING_FILE_NAME = "yarn-1.15.2+build.17-v2.tiny";
 	private static final String IGNORE_CLASS_PATH = "carpet.microtick";
-	private static final Map<String, String> mappings = Maps.newHashMap();
 
 	public static StackTraceElement[] deobfuscateStackTrace(StackTraceElement[] stackTraceElements)
 	{
-		return stackTraceElements;
+		List<StackTraceElement> list = Lists.newArrayList();
+		for (StackTraceElement element : stackTraceElements)
+		{
+			list.add(element);
+			if (element.getClassName().startsWith(IGNORE_CLASS_PATH))
+			{
+				list.clear();
+			}
+		}
+		list.add(0, new StackTraceElement("Stack trace", "", null, -1));
+		return list.toArray(new StackTraceElement[0]);
 	}
 }
