3c16af8c7a679feb0a04f31ca90dec0274dab280
Fallen_Breath <yqx791125833@gmail.com>
2020-10-31 11:19:50 +0800

    no movement for TISCM visualize projectile logger snowballs and optimized TISCM_VISPROJ_LOGGER removing

--- src/main/java/carpet/logging/logHelpers/TrajectoryLogHelper.java
+++ src/main/java/carpet/logging/logHelpers/TrajectoryLogHelper.java
@@ -4,16 +4,17 @@ import carpet.logging.Logger;
 import carpet.logging.LoggerRegistry;
 import carpet.utils.Messenger;
 import carpet.settings.CarpetSettings;
+import com.google.common.collect.Sets;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.projectile.EntitySnowball;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
-import org.apache.commons.lang3.ObjectUtils;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 /**
  * A generic log helper for logging the trajectory of things like blocks and throwables.
@@ -21,6 +22,8 @@ import java.util.List;
 public class TrajectoryLogHelper
 {
     private static final int MAX_TICKS_PER_LINE = 20;
+    public static final String TISCM_VISPROJ_LOGGER = "TISCM_VISPROJ_LOGGER";
+    public static final Set<EntitySnowball> VISUALIZE_SNOWBALLS = Sets.newHashSet();
 
     private boolean doLog;
     private Logger logger;
@@ -31,10 +34,12 @@ public class TrajectoryLogHelper
     private ArrayList<Long> gametick = new ArrayList<>();
     private String hitType;
     private World world;
+    private Entity entity;
 
-    public TrajectoryLogHelper(String logName)
+    public TrajectoryLogHelper(Entity entity, String logName)
     {
         this.logger = LoggerRegistry.getLogger(logName);
+        this.entity = entity;
         this.doLog = this.logger.hasOnlineSubscribers();
     }
 
@@ -57,12 +62,11 @@ public class TrajectoryLogHelper
         hitType = type;
         world = worldIn;
 
-        for (int i = 0; i < worldIn.loadedEntityList.size(); ++i){
-            Entity checkentity = worldIn.loadedEntityList.get(i);
-            if (checkentity.getTags().contains("TISCM_VISPROJ_LOGGER")){
-                worldIn.removeEntity(checkentity);
-            }
+        for (EntitySnowball entity : VISUALIZE_SNOWBALLS)
+        {
+            entity.getEntityWorld().removeEntity(entity);
         }
+        VISUALIZE_SNOWBALLS.clear();
     }
 
     public void onFinish()
@@ -83,7 +87,7 @@ public class TrajectoryLogHelper
                         String coll = collide.get(i);
                         long gt = gametick.get(i);
                         line.add("w  x");
-                        if (coll != "t") {
+                        if (!coll.equals("t")) {
                             line.add(String.format("^w Tick: %d\ngt: %d\nx: %f\ny: %f\nz: %f\n------------\nmx: %f\nmy: %f\nmz: %f",
                                     i, gt, pos.x, pos.y, pos.z, mot.x, mot.y, mot.z));
                         }
@@ -110,33 +114,48 @@ public class TrajectoryLogHelper
                     }
                     break;
                 case "visualize":
-                    if (CarpetSettings.visualizeProjectileLoggerEnabled){
-                    comp.add(Messenger.c("w visualize logger: visualized " + (positions.size() - 1) + " tick(s)"));
-                    for (int i = 0; i < positions.size(); i++) {
-                        Vec3d pos = positions.get(i);
-                        EntitySnowball visEntity = new EntitySnowball(world, pos.x, pos.y, pos.z);
-                        visEntity.setNoGravity(true);
-                        if (i < positions.size() - 1) {
-                            visEntity.setCustomName(new TextComponentString(i + ""));
-                        } else {
-                            visEntity.setCustomName(new TextComponentString("Hit"));
+                    if (CarpetSettings.visualizeProjectileLoggerEnabled) {
+                        if (!positions.isEmpty())
+                        {
+                            comp.add(Messenger.c("w visualize logger: visualized " + (positions.size() - 1) + " tick(s)"));
                         }
-                        visEntity.setCustomNameVisible(true);
-                        visEntity.addTag("TISCM_VISPROJ_LOGGER");
-                        visEntity.logHelper = null;
-                        visEntity.setInvulnerable(true);
-                        try {
-                            world.spawnEntity(visEntity);
-                        } catch (NullPointerException exception) {
-                            comp.clear();
-                            comp.add(Messenger.c("w visualize logger: visualize failed, someone is killing snowballs?"));
+                        for (int i = 0; i < positions.size(); i++) {
+                            Vec3d pos = positions.get(i);
+                            EntitySnowball visEntity = new EntitySnowball(world, pos.x, pos.y, pos.z);
+                            visEntity.setNoGravity(true);
+                            if (i < positions.size() - 1) {
+                                visEntity.setCustomName(new TextComponentString(i + ""));
+                            } else {
+                                visEntity.setCustomName(new TextComponentString("Hit"));
+                            }
+                            visEntity.setCustomNameVisible(true);
+                            visEntity.addTag(TISCM_VISPROJ_LOGGER);
+                            visEntity.logHelper = null;
+                            visEntity.setInvulnerable(true);
+                            try {
+                                world.spawnEntity(visEntity);
+                            } catch (NullPointerException exception) {
+                                comp.clear();
+                                comp.add(Messenger.c("w visualize logger: visualize failed, someone is killing snowballs?"));
+                            }
                         }
                     }
-                }
-                    else{
-                        comp.add(Messenger.c("w visualize logger: visualize is not enabled"));
-
-                }
+                    else
+                    {
+                        boolean warn = true;
+                        if (this.entity instanceof EntitySnowball)
+                        {
+                            EntitySnowball entitySnowball = (EntitySnowball)entity;
+                            if (entitySnowball.isVisProjLogger())
+                            {
+                                warn = false;
+                            }
+                        }
+                        if (warn)
+                        {
+                            comp.add(Messenger.c("w visualize logger: visualize is not enabled"));
+                        }
+                    }
                     break;
             }
             return comp.toArray(new ITextComponent[0]);
