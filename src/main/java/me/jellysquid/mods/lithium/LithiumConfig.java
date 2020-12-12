f5abe2c5ddd3803a44250c65eafe7555d372f584
Fallen_Breath <yqx791125833@gmail.com>
2020-12-13 03:00:18 +0800

    "switchable" lithium config

---
+++ src/main/java/me/jellysquid/mods/lithium/LithiumConfig.java
@@ -0,0 +1,19 @@
+package me.jellysquid.mods.lithium;
+
+public class LithiumConfig
+{
+	public static final boolean ALLOC_ENTITY_TRACKER = true;
+//	public static final boolean ALLOC_ENUM_VALUES = true;  // to complicated to be switchable
+	public static final boolean BLOCK_MOVING_BLOCK_SHAPES = true;
+	public static final boolean BLOCK_PISTON_SHAPES = true;
+	public static final boolean CACHED_HASHCODE = true;
+	public static final boolean ENTITY_DATA_TRACKER_USE_ARRAYS = true;
+	public static final boolean MATH_FAST_UTIL = true;
+	public static final boolean SHAPES_PRECOMPUTE_SHAPE_ARRAYS = true;
+	public static final boolean SHAPES_SHAPE_MERGING = true;
+	public static final boolean SHAPES_SPECIALIZED_SHAPES = true;
+	public static final boolean TAG = true;
+	public static final boolean WORLD_BLOCK_ENTITY_TICKING = true;
+	public static final boolean WORLD_EXPLOSIONS = true;
+	public static final boolean WORLD_TICK_SCHEDULER = true;
+}
