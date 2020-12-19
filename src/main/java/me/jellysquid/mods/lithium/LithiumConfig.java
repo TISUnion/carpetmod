1e5b49addb2526313f39cf52976f6b3fbca6c2ad
Fallen_Breath <yqx791125833@gmail.com>
2020-12-19 17:18:02 +0800

    one switch for lithium

--- src/main/java/me/jellysquid/mods/lithium/LithiumConfig.java
+++ src/main/java/me/jellysquid/mods/lithium/LithiumConfig.java
@@ -5,20 +5,23 @@ package me.jellysquid.mods.lithium;
  * Great thanks to lithium mod team for providing such OP optimizations
  * Lithium mod repos: https://github.com/jellysquid3/lithium-fabric
  */
+@SuppressWarnings("PointlessBooleanExpression")
 public class LithiumConfig
 {
-	public static final boolean ALLOC_ENTITY_TRACKER = true;
-//	public static final boolean ALLOC_ENUM_VALUES = true;  // to complicated to be switchable
-	public static final boolean BLOCK_MOVING_BLOCK_SHAPES = true;
-	public static final boolean BLOCK_PISTON_SHAPES = true;
-	public static final boolean CACHED_HASHCODE = true;
-	public static final boolean ENTITY_DATA_TRACKER_USE_ARRAYS = true;
-	public static final boolean MATH_FAST_UTIL = true;
-	public static final boolean SHAPES_PRECOMPUTE_SHAPE_ARRAYS = true;
-	public static final boolean SHAPES_SHAPE_MERGING = true;
-	public static final boolean SHAPES_SPECIALIZED_SHAPES = true;
-	public static final boolean TAG = true;
-	public static final boolean WORLD_BLOCK_ENTITY_TICKING = true;
-	public static final boolean WORLD_EXPLOSIONS = true;
-	public static final boolean WORLD_TICK_SCHEDULER = true;
+	private static final boolean LITHIUM_ENABLE = true;
+
+	public static final boolean ALLOC_ENTITY_TRACKER            = LITHIUM_ENABLE && true;
+//	public static final boolean ALLOC_ENUM_VALUES               = LITHIUM_ENABLE && true;  // to complicated to be switchable
+	public static final boolean BLOCK_MOVING_BLOCK_SHAPES       = LITHIUM_ENABLE && true;
+	public static final boolean BLOCK_PISTON_SHAPES             = LITHIUM_ENABLE && true;
+	public static final boolean CACHED_HASHCODE                 = LITHIUM_ENABLE && true;
+	public static final boolean ENTITY_DATA_TRACKER_USE_ARRAYS  = LITHIUM_ENABLE && true;
+	public static final boolean MATH_FAST_UTIL                  = LITHIUM_ENABLE && true;
+	public static final boolean SHAPES_PRECOMPUTE_SHAPE_ARRAYS  = LITHIUM_ENABLE && true;
+	public static final boolean SHAPES_SHAPE_MERGING            = LITHIUM_ENABLE && true;
+	public static final boolean SHAPES_SPECIALIZED_SHAPES       = LITHIUM_ENABLE && true;
+	public static final boolean TAG                             = LITHIUM_ENABLE && true;
+	public static final boolean WORLD_BLOCK_ENTITY_TICKING      = LITHIUM_ENABLE && true;
+	public static final boolean WORLD_EXPLOSIONS                = LITHIUM_ENABLE && true;
+	public static final boolean WORLD_TICK_SCHEDULER            = LITHIUM_ENABLE && true;
 }
