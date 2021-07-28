2078bf8e01c46b70e9cceeab78771f7fb694a32e
Fallen_Breath <yqx791125833@gmail.com>
2021-07-29 03:23:43 +0800

    village command details

--- src/main/java/carpet/commands/VillageCommand.java
+++ src/main/java/carpet/commands/VillageCommand.java
@@ -13,7 +13,6 @@ import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.event.ClickEvent;
 import net.minecraft.village.Village;
 import net.minecraft.village.VillageDoorInfo;
-import net.minecraft.world.World;
 import net.minecraft.world.WorldServer;
 
 import javax.annotation.Nullable;
@@ -58,7 +57,7 @@ public class VillageCommand
 				for (int i = 0; i < villageList.size(); i++)
 				{
 					Village village = villageList.get(i);
-					generateVillageInfo(messages, village, i);
+					generateVillageInfo(messages, village, i, false);
 				}
 			}
 			if (messages.isEmpty())
@@ -71,7 +70,7 @@ public class VillageCommand
 			List<Village> villageList = source.getWorld().getVillageCollection().getVillageList();
 			if (0 <= villageId && villageId < villageList.size())
 			{
-				generateVillageInfo(messages, villageList.get(villageId), villageId);
+				generateVillageInfo(messages, villageList.get(villageId), villageId, true);
 			}
 			else
 			{
@@ -82,9 +81,15 @@ public class VillageCommand
 		return 1;
 	}
 
-	private static void generateVillageInfo(List<ITextComponent> messages, Village village, int index)
+	private static void generateVillageInfo(List<ITextComponent> messages, Village village, int index, boolean showDoorDetail)
 	{
-		messages.add(Messenger.s(String.format("Village #%d", index)));
+		messages.add(TextUtil.getFancyText(
+				null,
+				Messenger.s(String.format("Village #%d", index)),
+				Messenger.s(String.format("Click to see full information of village #%d\nYou need to be in the same dimension of this village", index)),
+				new ClickEvent(ClickEvent.Action.RUN_COMMAND, String.format("/village info %d", index))
+		));
+		messages.add(Messenger.c("w   Dimension: ", TextUtil.getDimensionNameText(village.getWorld().getDimension().getType())));
 		BlockPos center = village.getCenter();
 		messages.add(TextUtil.getFancyText(
 				null,
@@ -96,29 +101,32 @@ public class VillageCommand
 		messages.add(Messenger.s(String.format("  VillagerCount: %d", village.getNumVillagers())));
 		messages.add(Messenger.s(String.format("  DoorCount: %d", village.getVillageDoorInfoList().size())));
 		List<ITextComponent> line = Lists.newArrayList();
-		int dotCount = 0;
 		List<VillageDoorInfo> villageDoorInfoList = village.getVillageDoorInfoList();
-		for (int j = 0; j < villageDoorInfoList.size(); j++)
+		for (int i = 0; i < villageDoorInfoList.size(); i++)
 		{
-			VillageDoorInfo doorInfo = villageDoorInfoList.get(j);
+			VillageDoorInfo doorInfo = villageDoorInfoList.get(i);
 			BlockPos pos = doorInfo.getDoorBlockPos();
 			line.add(TextUtil.getFancyText(
 					"g",
 					Messenger.s(" x"),
 					Messenger.c(
-							String.format("w Door #%s\n", j + 1),
+							String.format("w Door #%s\n", i + 1),
 							"w ------------\n",
 							String.format("w X: %d\n", pos.getX()),
 							String.format("w Y: %d\n", pos.getY()),
 							String.format("w Z: %d", pos.getZ())
 					),
-					new ClickEvent(ClickEvent.Action.RUN_COMMAND, TextUtil.getTeleportCommand(pos, village.getWorld().getDimension().getType()))
+					new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, TextUtil.getTeleportCommand(pos, village.getWorld().getDimension().getType()))
 			));
-			dotCount++;
-			if (dotCount == 20 || j == villageDoorInfoList.size() - 1)
+			if (line.size() == 20 || i == villageDoorInfoList.size() - 1)
 			{
 				messages.add(Messenger.c("w    ", Messenger.c(line.toArray(new Object[0]))));
 				line.clear();
+			}
+			if (!showDoorDetail && i == 2)
+			{
+				messages.add(Messenger.c("w    ", Messenger.c(line.toArray(new Object[0])), "g  ..."));
+				break;
 			}
 		}
 	}
