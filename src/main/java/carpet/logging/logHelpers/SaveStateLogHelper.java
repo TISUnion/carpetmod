122137341ea1c72f069d5676531173d7cea07f15
Fallen_Breath <yqx791125833@gmail.com>
2021-12-25 21:08:56 +0800

    Make sure savestate logger logs on server thread
    instead of file io thread

--- src/main/java/carpet/logging/logHelpers/SaveStateLogHelper.java
+++ src/main/java/carpet/logging/logHelpers/SaveStateLogHelper.java
@@ -1,5 +1,6 @@
 package carpet.logging.logHelpers;
 
+import carpet.CarpetServer;
 import carpet.logging.LoggerRegistry;
 import carpet.utils.Messenger;
 import net.minecraft.util.math.ChunkPos;
@@ -23,8 +24,10 @@ public class SaveStateLogHelper
 		{
 			return;
 		}
-		LoggerRegistry.getLogger("savestate").log(() -> new ITextComponent[]{
-				Messenger.s(String.format("Chunk [%d, %d] %.2fKB (%.2f%%)%s", chunkPos.x, chunkPos.z, size / 1024.0D, radio * 100, radio >= 1 ? " OVERFLOW" : ""))
+		CarpetServer.minecraft_server.addScheduledTask(() -> {
+			LoggerRegistry.getLogger("savestate").log(() -> new ITextComponent[]{
+					Messenger.s(String.format("Chunk [%d, %d] %.2fKB (%.2f%%)%s", chunkPos.x, chunkPos.z, size / 1024.0D, radio * 100, radio >= 1 ? " OVERFLOW" : ""))
+			});
 		});
 	}
 }
