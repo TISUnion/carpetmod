6859b68d66a7c3764f39dfc95256b106472e7269
Fallen_Breath <yqx791125833@gmail.com>
2022-12-13 17:13:58 +0800

    Fixed crash on newer java version due to reflection at `java.lang.Thread`

--- src/main/java/carpet/logging/threadstone/GlassThreadUtil.java
+++ src/main/java/carpet/logging/threadstone/GlassThreadUtil.java
@@ -1,50 +1,22 @@
 package carpet.logging.threadstone;
 
 import com.mojang.datafixers.util.Pair;
-import net.minecraft.util.Util;
-
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.function.Supplier;
+import org.apache.commons.lang3.ThreadUtils;
 
 public class GlassThreadUtil
 {
-	// Thread.getAllStackTraces().keySet()
-	private static final Supplier<Thread[]> getThreadsSupplier = Util.make(() -> {
-		try
-		{
-			Method method = Thread.class.getDeclaredMethod("getThreads");
-			method.setAccessible(true);
-			return () -> {
-				try
-				{
-					return (Thread[])method.invoke(null);
-				}
-				catch (IllegalAccessException | InvocationTargetException e)
-				{
-					throw new RuntimeException(e);
-				}
-			};
-		}
-		catch (NoSuchMethodException e)
-		{
-			throw new RuntimeException(e);
-		}
-	});
-
 	public static Pair<Integer, Integer> getGlassThreadCount()
 	{
 		int runnable = 0;
 		int total = 0;
-		for (Thread thread : getThreadsSupplier.get())
+
+		// don't use Thread.getAllStackTraces().keySet(), since that's very slow
+		for (Thread thread : ThreadUtils.findThreads(t -> t.getName().startsWith("Downloader ")))
 		{
-			if (thread.getName().startsWith("Downloader "))
+			total++;
+			if (thread.getState() == Thread.State.RUNNABLE)
 			{
-				total++;
-				if (thread.getState() == Thread.State.RUNNABLE)
-				{
-					runnable++;
-				}
+				runnable++;
 			}
 		}
 		return Pair.of(runnable, total);
