f63d22f18dadd5f13cab2a0e93264532a6bfe239
Fallen_Breath <yqx791125833@gmail.com>
2023-03-16 01:49:52 +0800

    cooler instantfall logger message color

--- src/main/java/carpet/logging/instantfall/InstantFallLogger.java
+++ src/main/java/carpet/logging/instantfall/InstantFallLogger.java
@@ -3,6 +3,7 @@ package carpet.logging.instantfall;
 import carpet.logging.AbstractLogger;
 import carpet.utils.Messenger;
 import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextFormatting;
 
 public class InstantFallLogger extends AbstractLogger
 {
@@ -21,6 +22,9 @@ public class InstantFallLogger extends AbstractLogger
 
 	public void onInstantFallFlagFlipped(boolean currentFlag)
 	{
-		this.log(() -> new ITextComponent[]{advTr("flag_changed", "InstantFall flag has changed to %s", Messenger.bool(currentFlag))});
+		this.log(() -> new ITextComponent[]{Messenger.formatting(
+				advTr("flag_changed", "InstantFall flag changed to %s", Messenger.bool(currentFlag)),
+				TextFormatting.LIGHT_PURPLE, TextFormatting.ITALIC
+		)});
 	}
 }
